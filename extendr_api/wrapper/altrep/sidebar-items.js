c934a63a6b9869e33ca5b99bc44559d51df0e18c
brancengregory <brancengregory@users.noreply.github.com>
2025-04-18 03:20:58 +0000

    deploy: d4478e6ad38b8e5f006072a399299065d19af985

---
+++ extendr_api/wrapper/altrep/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["manifest"],"macro":["make_from_iterator"],"struct":["Altrep"],"trait":["AltComplexImpl","AltIntegerImpl","AltListImpl","AltLogicalImpl","AltRawImpl","AltRealImpl","AltStringImpl","AltrepImpl"]};
